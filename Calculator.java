7dab5b075d2f6966fd931f4d4b24459317e5fcb7
SusmithaHS <susmithahs24@gmail.com>
2025-06-23 12:52:10 +0530

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class Calculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static double divide(double a, double b) {
+        if (b != 0) return a / b;
+        else {
+            System.out.println("Error: Division by zero!");
+            return 0;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean continueCalc = true;
+        while (continueCalc) {
+            System.out.print("Enter first number: ");
+            double num1 = sc.nextDouble();
+            System.out.print("Enter operator (+, -, *, /): ");
+            char operator = sc.next().charAt(0);
+            System.out.print("Enter second number: ");
+            double num2 = sc.nextDouble();
+            double result;
+            switch (operator) {
+                case '+':
+                    result = add(num1, num2);
+                    break;
+                case '-':
+                    result = subtract(num1, num2);
+                    break;
+                case '*':
+                    result = multiply(num1, num2);
+                    break;
+                case '/':
+                    result = divide(num1, num2);
+                    break;
+                default:
+                    System.out.println("Invalid operator!");
+                    continue;
+            }
+            System.out.println("Result: " + result);
+            System.out.print("Do you want to continue? (yes/no): ");
+            String choice = sc.next();
+            if (!choice.equalsIgnoreCase("yes")) {
+                continueCalc = false;
+                System.out.println("Exiting Calculator. Goodbye!");
+            }
+        }
+
+        sc.close();
+    }
+}
+
